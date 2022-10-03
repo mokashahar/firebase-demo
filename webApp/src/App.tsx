84753fe597d1f0a4a8f1236b4fee38d5cd20eaeb
shahar.moka <mokashahar@gmail.com>
2022-10-03 16:43:08 +0300

    minor

--- webApp/src/App.tsx
+++ webApp/src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import { Box, Button, ButtonGroup, Stack } from '@mui/material';
-import { getValue } from 'firebase/remote-config';
-import { remoteConfig } from './services/firebase/firebase';
 import FireStore from './features/FireStore/FireStore';
 
 function App() {
   const [view, setView] = useState<string>();
-  console.log('VALUE: ', getValue(remoteConfig, 'MaxLimit').asString());
+  //console.log('VALUE: ', getValue(remoteConfig, 'MaxLimit').asString());
 
   return (
     <Box width='100%' height='100%' position='relative'>
