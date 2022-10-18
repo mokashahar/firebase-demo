03db00a4176fc803ccd7e50d99bfbf716ab3e935
shahar.moka <mokashahar@gmail.com>
2022-10-18 17:18:03 +0300

    minor

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,8 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs', { structuredData: true });
+  functions.logger.info('Hello log', { structuredData: true });
   response.send('Hello from Firebase!');
 });
 
