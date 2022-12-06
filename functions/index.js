63ed372fe04d5c0c0d181cdcbfaca8c170f410bd
shahar.moka <mokashahar@gmail.com>
2022-12-06 17:26:54 +0200

    minor

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,7 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
   // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({ originl: original });
+  const writeResult = await admin.firestore().collection('messages').add({ msg: original });
   // Send back a message that we've successfully written the message
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
